215067eec2f902e371d22ad1f0d42c06cf39c126
SidonTheTroll <70560640+SidonTheTroll@users.noreply.github.com>
2023-10-11 21:27:05 +0530

    removed useless file

--- gh-retina-test.sh
+++ gh-retina-test.sh
@@ -23,97 +23,17 @@ readonly JSON_KEYS=(
     "updated_at"
 )
 
-
-_get_avatar_url ()
-{
-  local username="$1"
-  local avatar_url
-  avatar_url="$(_get_user_info "$username" | jq -r .avatar_url)"
-  echo "$avatar_url"
+function _get_user_info() {
+    local username="$1"
+    gh api "users/$username"
 }
 
-function _display_profile_picture() {
+function _get_avatar_url() {
     local username="$1"
     local avatar_url
-    avatar_url="$(_get_avatar_url "$username")"
-
-    if [ "$avatar_url" != "null" ]; then
-        # Display the profile picture in Kitty terminal on the left
-        kitty +kitten icat --align left "$avatar_url"
-    else
-        echo "User not found or has no profile picture."
-    fi
+    avatar_url="$(_get_user_info "$username" | jq -r .avatar_url)"
+    echo "$avatar_url"
 }
-
-
-_user_pfp ()
-{
-  echo "kitty +kitten icat --align left "$avatar_url""
-}
-
-
-_main ()
-{
-  paste -d " " <(echo -e "${_user_pfp}") <(gh api "user" | _print_info)
-}
-
-_get_value ()
-{
-  jq -r ".${1}" </dev/stdin
-}
-
-_get_user_info ()
-{
-  local username="$1"
-  gh api "users/$username"
-}
-
-_print_info ()
-{
-  local userinfo
-  userinfo="$(cat /dev/stdin)"
-
-  echo -e "\n"
-  echo -e "${COLOR_BG_BLACK} $(_get_value "login" <<< "${userinfo}") ${COLOR_NONE}"
-
-  echo -e ""
-
-  _get_value "bio" <<< ${userinfo}
-
-  _print_bar 
-
-  for k in "${JSON_KEYS[@]}"; do
-        echo -e "${COLOR_FG_HIGHLIGHT} ${k} ${COLOR_NONE}${INDICATOR} $(_get_value "${k}" <<<"${userinfo}")"
-    done
-
-  _print_bar 
-
-  _print_color_bar
-}
-
-
-
-
-
-
-function _print_bar() {
-    echo -e "${COLOR_FG_BLACK}=================================================${COLOR_NONE}"
-}
-
-function _print_color_bar() {
-    echo -e ""
-
-    for i in "" ";1"; do
-        for j in $(seq 0 7); do
-            printf " \033[4${j}${i}m    \033[m"
-        done
-        echo -e "\n"
-    done
-
-    echo -e ""
-}
-
-
 
 _main "${@}"
 exit "${?}"
